dc2a7914b6c6073a3d350d8cf175219dd8e47b39
Michael Lyons <mdl0394@gmail.com>
2015-05-19 18:12:57 -0700

    Added heroku.js, Procfile and database

--- config/database.js
+++ config/database.js
@@ -23,7 +23,13 @@
             database_native: false
         },
         development: {
-
+            database_name: 'rndmlnk_development',
+            database_username: 'rndmlnk',
+            database_password: 'rndmlnk',
+            database_port: 5432,
+            database_url: 'localhost',
+            sequelize_logging: false,
+            database_native: false
         },
         heroku: {
             sequelize_logging: console.log,
