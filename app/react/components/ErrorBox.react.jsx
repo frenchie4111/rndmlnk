0be9097f06909dbd3376ae50f827ec815b8b327d
Michael Lyons <mdl0394@gmail.com>
2015-05-22 17:04:12 -0700

    Added ErrorBox

---
+++ app/react/components/ErrorBox.react.jsx
@@ -0,0 +1,41 @@
+/**
+ * Copyright of Michael Lyons
+ *
+ * Authors:
+ *     - Michael Lyons (mdl0394@gmail.com)
+ */
+
+(function() {
+    'use strict';
+
+    var React = require( 'react' ),
+        _ = require( 'underscore' );
+
+    var Bar = require( './Bar.react.jsx' );
+
+    var ErrorBox;
+
+    module.exports = ErrorBox = React.createClass( {
+        getDefaultProps: function() {
+            return {
+                style: {
+                    backgroundColor: '#3498DB'
+                },
+                table_cell_style: {}
+            };
+        },
+        _style: {
+            bar: {
+                backgroundColor: '#E74C3C'
+            }
+        },
+        render: function() {
+            return (
+                <Bar
+                    style={ this._style.bar }>
+                    An Error Has Occurred
+                </Bar>
+            );
+        }
+    } );
+})();
